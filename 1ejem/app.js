f1a7e4dab37e7956f073b5b5520a593fb89fdfba
´´ALEXANDR7353´´ <zapataalexander693@gmail.com>
2024-09-20 15:58:04 +0200

    ejercisio1.2

---
+++ 1ejem/app.js
@@ -0,0 +1,31 @@
+// EJERCISIO 1 
+
+let myFavoriteHero = "hulk";
+let x = 50;
+let h = 5;
+let y = 10;
+let z = h + y;
+
+// EJERCIO 2 
+
+1.1
+console.log(10 * 5);
+1.2
+console.log(10 / 2);
+1.3
+console.log(15 / 9);
+1.4
+let p = 10;
+let j = 5;
+let o = 15;
+ console.log(o) = "(p + j)";
+ 1.5
+ let c = 10;
+ let m = 5;
+ let i = 50;
+ console.log(c) = "(c * m)";
+ 
+ 
+
+
+
