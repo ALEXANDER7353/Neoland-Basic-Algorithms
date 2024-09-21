6f7ce7c7497870b12108d6cd3e31289da9be114a
´´ALEXANDR7353´´ <zapataalexander693@gmail.com>
2024-09-21 19:29:22 +0200

    ejer1

--- 1ejem/app.js
+++ 1ejem/app.js
@@ -8,25 +8,67 @@ let z = h + y;
 
 // EJERCIO 2 
 
-1.1
+///1.1
 console.log(10 * 5);
-1.2
+///1.2
 console.log(10 / 2);
-1.3
+///1.3
 console.log(15 % 9);
 
-1.4
+///1.4
 let p = 10;
 let j = 5;
 let o = 15;
  console.log(p + j);
- 1.5
+ ///1.5
  let c = 10;
  let m = 5;
  let i = 50;
  console.log(c * m);
  
  
+/// ejercio 2 
+
+///1.1
+let userage = 25;
+
+
+const character =()=>{
+    let userage = 25; 
+
+
+
+};
+
+///1.2
+
+let firstName = 'Jon'; 
+let lastName = 'Snow'; 
+let age = 24; 
+
+function sayHi() {
+    console.log(`soy, ${firstName}`, `${lastName}`); (`tengo, ${age}`); (`y me gustan los lobos`);
+};
+
+///1.3
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
