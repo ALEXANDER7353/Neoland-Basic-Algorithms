4589effffc0a12d82de296951e84e3f7806d0102
´´ALEXANDR7353´´ <zapataalexander693@gmail.com>
2024-09-28 20:58:15 +0200

    ejem9

--- 1ejem/app.js
+++ 1ejem/app.js
@@ -168,10 +168,41 @@ console.log(number2 / 2 ) || (number1 / 5);
 
 ///1.1 Crea un bucle for que vaya desde 0 a 9 y muestralo por consola.
 
-let numeros = "()"
-
-for  ( let numeros = 0 ; numeros < 10; numeros++){
-    console.log(numeros);
+for (let i = 0; i< 10; i++) {
+    console.log(i);
     
+  
 }
-console.log(numeros);
+  
+
+///1.2 Crea un bucle for que vaya desde 0 a 9 y muestralo por consola solo 
+///cuando el resto del numero dividido entre 2 sea 0.
+
+for (let i = 0; i< 10; i++) {
+    if (i % 2 === 0 ) {
+        console.log(i );
+        
+        
+    }
+}
+
+  ///crea un bucle para conseguir dormir contando ovejas. 
+///Este bucle tiene que dar 10 vueltas, es decir, 10 console.log.
+///Muestra por consola un mensaje diciendo 'Intentando dormir 🐑' en cada vuelta del bucle 
+///y cambia el mensaje en la décima vuelta a 'Dormido!'.
+
+
+    
+  
+
+
+
+
+        
+        
+
+    
+
+
+    
+  
