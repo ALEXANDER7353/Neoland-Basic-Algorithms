0f8911b5a45350a30a714e54d2ddc085ea269f57
´´ALEXANDR7353´´ <zapataalexander693@gmail.com>
2024-10-04 13:42:46 +0200

    loops

--- tareas/app2.js
+++ tareas/app2.js
@@ -8,17 +8,31 @@
 const products = ['Camiseta de Pokemon', 'Pantalón coquinero', 'Gorra de gansta',
      'Camiseta de Basket', 'Cinrurón de Orión', 'AC/DC Camiseta']
      
-    
+     let conLetraS = products.filter(e => e.includes('Camiseta'))
+     console.log(conLetraS); // [ 'casa', 'mesa' ]
+     
        
 
 
+ //2: Condicionales avanzados
+
+ //Comprueba en cada uno de los usuarios que tenga al menos dos trimestres aprobados y añade la propiedad isApproved 
+ //a true o false en consecuencia. Una vez lo tengas compruébalo con un console.log. 
+
+ const alumns = [
+     {name: 'Pepe Viruela', T1: false, T2: false, T3: true}, 
+           {name: 'Lucia Aranda', T1: true, T2: false, T3: true},
+           {name: 'Juan Miranda', T1: false, T2: true, T3: true},
+           {name: 'Alfredo Blanco', T1: false, T2: false, T3: false},
+           {name: 'Raquel Benito', T1: true, T2: true, T3: true}]
+
+          
+      
 
 
 
 
-     //2: Condicionales avanzados
 
-  
 
       //3: Probando For...of
 
@@ -48,7 +62,7 @@ const products = ['Camiseta de Pokemon', 'Pantalón coquinero', 'Gorra de gansta
            weight: '259kg'
      };
      for (const claves in alien) {
-          console.log(`estos son los:  ${alien[claves]}`);
+          console.log(`estas son las claves :  ${alien[claves]}`);
           
         
      }
@@ -58,9 +72,12 @@ const products = ['Camiseta de Pokemon', 'Pantalón coquinero', 'Gorra de gansta
      //Usa un bucle for para recorrer todos los destinos del array y elimina los elementos que tengan el id 11 y 40.
      // Imprime en un console log el array. Puedes usar este array:
 
-     const placesToTravel1 = [{id: 5, name: 'Japan'}, {id: 11, name: 'Venecia'}, {id: 23, name: 'Murcia'},
+     const elementos = [{id: 5, name: 'Japan'}, {id: 11, name: 'Venecia'}, {id: 23, name: 'Murcia'},
            {id: 40, name: 'Santander'}, {id: 44, name: 'Filipinas'}, {id: 59, name: 'Madagascar'}]
            
-           const resultado = placesToTravel.filter(e => e.numero != 40  );
-           
-           console.log(resultado);
+           console.log(elementos, elementos.length)
+
+delete elementos[1] 
+delete elementos [3]
+
+console.log(elementos, elementos.length)
