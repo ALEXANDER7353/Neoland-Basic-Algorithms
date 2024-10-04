03a22560a61cb3a544fa92350158f1cfc61359dd
´´ALEXANDR7353´´ <zapataalexander693@gmail.com>
2024-10-04 18:23:32 +0200

    loops

--- tareas/app2.js
+++ tareas/app2.js
@@ -9,8 +9,7 @@ const products = ['Camiseta de Pokemon', 'Pantalón coquinero', 'Gorra de gansta
      'Camiseta de Basket', 'Cinrurón de Orión', 'AC/DC Camiseta']
      
      let conLetraS = products.filter(e => e.includes('Camiseta'))
-     console.log(conLetraS); // [ 'casa', 'mesa' ]
-     
+     console.log(conLetraS); 
        
 
 
@@ -19,6 +18,7 @@ const products = ['Camiseta de Pokemon', 'Pantalón coquinero', 'Gorra de gansta
  //Comprueba en cada uno de los usuarios que tenga al menos dos trimestres aprobados y añade la propiedad isApproved 
  //a true o false en consecuencia. Una vez lo tengas compruébalo con un console.log. 
 
+ 
  const alumns = [
      {name: 'Pepe Viruela', T1: false, T2: false, T3: true}, 
            {name: 'Lucia Aranda', T1: true, T2: false, T3: true},
@@ -26,9 +26,9 @@ const products = ['Camiseta de Pokemon', 'Pantalón coquinero', 'Gorra de gansta
            {name: 'Alfredo Blanco', T1: false, T2: false, T3: false},
            {name: 'Raquel Benito', T1: true, T2: true, T3: true}]
 
-          
-      
+     
 
+      
 
 
 
@@ -47,13 +47,8 @@ const products = ['Camiseta de Pokemon', 'Pantalón coquinero', 'Gorra de gansta
       
 
  
-     
 
-          
-     
-
-
-     //4: Probando For...in
+   //4: Probando For...in
 
      const alien = {
           name: 'Wormuck',
@@ -74,10 +69,30 @@ const products = ['Camiseta de Pokemon', 'Pantalón coquinero', 'Gorra de gansta
 
      const elementos = [{id: 5, name: 'Japan'}, {id: 11, name: 'Venecia'}, {id: 23, name: 'Murcia'},
            {id: 40, name: 'Santander'}, {id: 44, name: 'Filipinas'}, {id: 59, name: 'Madagascar'}]
-           
-           console.log(elementos, elementos.length)
+  
+     let menoselemtos=elementos.splice (1,1)
 
-delete elementos[1] 
-delete elementos [3]
+     let nuevosDeportes = elementos.splice (2,1)
+ 
+     console.log(elementos);
 
-console.log(elementos, elementos.length)
+
+    // 6: Mixed For...of e includes
+
+//Usa un bucle for...of para recorrer todos los juguetes y elimina los que incluyan la palabra gato.
+ //Recuerda que puedes usar la función.includes() para comprobarlo.Puedes usar este array:
+ const toys = [
+     {id: 5, name: 'Buzz MyYear'}, 
+     {id: 11, name: 'Action Woman'}, 
+     {id: 23, name: 'Barbie Man'}, 
+     {id: 40, name: 'El gato con Guantes'},
+     {id: 40, name: 'El gato felix'}
+     ]
+
+for (const element of toys) {
+     let menostoys = toys.splice(3,4)
+     
+}
+console.log(toys);
+
+
